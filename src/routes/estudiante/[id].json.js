e93e43ccf9f089e54b94bd6ea9c307f40ff4f591
Pau Fernández <pau.fernandez@upc.edu>
2019-11-15 12:43:03 +0100

    Mejora del endpoint de estudiante/[id]

--- src/routes/estudiante/[id].json.js
+++ src/routes/estudiante/[id].json.js
@@ -5,35 +5,48 @@ export function get(req, res, next) {
     const id = req.params.id;
 
     const jsonResponse = (code, obj) => {
-        res.writeHead(code, {'Content-Type': 'application/json'});
+        res.writeHead(code, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(obj))
     }
 
     db.get(`SELECT estudiantes.*, universidades.universidad, universidades.pais, universidades.codigo_universidad 
-            FROM estudiantes, universidades WHERE estudiantes.universidad = universidades.codigo_universidad AND email = ?`, [id], (err, row) => {
-        const estudiante = row;
-        db.all(`SELECT acuerdos_academicos.*, periodos_academicos.año, periodos_academicos.cuatrimestre, titulaciones.nombre_catalan, titulaciones.nombre_castellano, titulaciones.nombre_ingles 
-            FROM acuerdos_academicos, periodos_academicos, titulaciones WHERE acuerdos_academicos.periodo_academico = periodos_academicos.id_periodo AND acuerdos_academicos.titulacion = titulaciones.codigo_titulacion AND acuerdos_academicos.estudiante = ?`, [id],
-        (err, rows) => {
-            if(err) {
+            FROM estudiantes, universidades WHERE estudiantes.universidad = universidades.codigo_universidad AND email = ?`, [id],
+        (err, estudiante) => {
+            if (err) {
                 console.log(err)
-                jsonResponse(500, { error: `Cannot get subject ${id}: ${err}` })
+                jsonResponse(500, { error: `No he podido consultar el estudiante (${id}): ${err}` })
                 return
-        }      
-        estudiante.acuerdos = rows;
-    })
-        db.all(`SELECT asignaturas.*, ofertas.*, asignaciones.*, acuerdos_academicos.* 
-                FROM asignaturas INNER JOIN ofertas, asignaciones, acuerdos_academicos 
-                ON acuerdos_academicos.estudiante = ? AND acuerdos_academicos.id_acuerdo = asignaciones.acuerdo_academico 
-                AND asignaciones.oferta = ofertas.id_oferta AND ofertas.asignatura = asignaturas.codigo_asignatura`, [id],
-        (err, rows) => {
-            if(err) {
-                console.log(err)
-                jsonResponse(500, { error: `Cannot get subject ${id}: ${err}` })
-                return
-        }      
-        estudiante.asignaturas = rows;
-        jsonResponse(200, estudiante);
-    })
-    })
+            }
+            db.all(`SELECT a.*, p.año, p.cuatrimestre, t.nombre_catalan, t.nombre_castellano, t.nombre_ingles 
+                  FROM acuerdos_academicos as a, periodos_academicos as p, titulaciones as t 
+                WHERE a.periodo_academico = p.id_periodo 
+                  AND a.titulacion = t.codigo_titulacion 
+                  AND a.estudiante = ?`, [id],
+                (err, acuerdos) => {
+                    if (err) {
+                        console.log(err)
+                        jsonResponse(500, { error: `No he podido consultar los acuerdos (${id}): ${err}` })
+                        return
+                    }
+                    db.all(`SELECT asignaturas.*, ofertas.*, asignaciones.*, ac.* 
+                            FROM asignaturas INNER JOIN ofertas, asignaciones, acuerdos_academicos as ac
+                            ON ac.estudiante = ? AND 
+                               ac.id_acuerdo = asignaciones.acuerdo_academico AND 
+                               asignaciones.oferta = ofertas.id_oferta AND 
+                               ofertas.asignatura = asignaturas.codigo_asignatura`, [id],
+                        (err, asignaturas) => {
+                            if (err) {
+                                console.log(err)
+                                jsonResponse(500, { error: `No he podido consultar las asignaturas (${id}): ${err}` })
+                                return
+                            }
+                            jsonResponse(200, {
+                                ...estudiante,
+                                acuerdos,
+                                asignaturas,
+                            });
+                        })
+                })
+
+        })
 }
