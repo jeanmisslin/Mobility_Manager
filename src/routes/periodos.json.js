0b1ce9184394648b5cd8d3518a1abebaede246e8
jeanmisslin <jeanmisslindj@gmail.com>
2019-11-16 19:49:53 +0100

    Endpoint Periodos

--- src/routes/periodos.json.js
+++ src/routes/periodos.json.js
@@ -1,18 +1,19 @@
 import { db } from '../db'
 
 export function get(req, res, next) {
+  const jsonResponse = (code, obj) => {
+    res.writeHead(code, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(obj))
+  }
 
-    const jsonResponse = (code, obj) => {
-        res.writeHead(code, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(obj))
+  db.all(`SELECT periodos_academicos.* FROM periodos_academicos`, (err, periodos) => {
+    if (err) {
+      console.log(err)
+      jsonResponse(500, { error: `No se pueden consultar periodos_academicos: ${err}` })
+      return
     }
-
-    db.all(`SELECT * FROM periodos_academicos`, (err, rows)=> {
-        if(err) {
-            console.log(err)
-            jsonResponse(500, { error: `Cannot get periodos_academicos: ${err}` })
-            return
-        }
-        jsonResponse(200, rows);
+    jsonResponse(200, {
+      periodos,
     })
+  })
 }
