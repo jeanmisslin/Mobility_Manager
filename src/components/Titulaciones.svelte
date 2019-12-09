84571d7eda64afd51986da74b5d78c9307c94650
jeanmisslin <jeanmisslindj@gmail.com>
2019-12-09 11:09:25 +0100

    Componente Titulaciones
    La idea era tener un componente que con un each llamara al componente titulacion y mostrara las asignaturas de todas las titulaciones. Al generare un each de titulaciones as t, no me reconoce t.codigo_titulacion y me veo obligado a llamar al componente titulacion indicando el codigo manualmente.

---
+++ src/components/Titulaciones.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Titulacion from "./Titulacion.svelte";
+
+  export let ofertas;
+  export let titulaciones;
+
+  let { codigo_titulacion, nombre_ingles } = titulaciones;
+</script>
+
+<Titulacion {ofertas} {titulaciones} codigo ='320GRESAUD'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='320GREMECA'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='320GREQUIM	'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='320GREELEC	'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='320GREDIDP'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='320GREEIA'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='320GRETDT'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='220GRETIND'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='220GRETAER'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='220GREVAER'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='220MEI'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='220MUESAI'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='220MUAERON'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='220MEORSEM'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='205MEM'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='205MASE'/>
+
+<Titulacion {ofertas} {titulaciones} codigo ='205METEXPA'/>
+
+
