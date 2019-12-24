21363b33a83958f57558463c3183afca416b7a53
jeanmisslin <jeanmisslindj@gmail.com>
2019-12-24 14:59:10 +0100

    Componente RestarPlazaDisponible
    No funciona. Su proposito es ejecutarse cuando se asigna una asignatura con el estado concedida para restar su plaza del campo ofertas.plazas_disponibles.

---
+++ src/components/RestarPlazaDisponible.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let oferta;
+
+  let modificacion = {
+      disponibles: oferta.plazas_disponibles-1,
+      concedidas: oferta.plazas_concedidas+1,
+      asignatura: oferta.asignatura
+  }
+
+  function restar() {
+    fetch(`restarplazadisponible.json`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(modificacion)
+    })
+      .then(body => body.json())
+      .then(json => {
+        if (json.error) {
+          message = json.error;
+        } else {
+          message = "modificacion guardada";
+        }
+      });
+  }
+</script>
+
+{restar({modificacion})}
