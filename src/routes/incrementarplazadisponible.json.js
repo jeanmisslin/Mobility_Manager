3e09e12bb7dc4b26062d6ef2e79ba93e4c9740a2
jeanmisslin <jeanmisslindj@gmail.com>
2019-12-27 18:41:44 +0100

    Incrementarplazadisponible
    Genera la sentencia SQL que permite liberar una plaza cuando se modifica el estado de una asignación.

---
+++ src/routes/incrementarplazadisponible.json.js
@@ -0,0 +1,33 @@
+
+import { db } from '../db'
+
+export function post(req, res, next) {
+    const jsonResponse = (code, obj) => {
+        res.writeHead(code, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(obj))
+    }
+
+    let body = '';
+    req.on('data', data => body += data)
+    req.on('end', () => {
+        const modificacion = JSON.parse(body)
+        db.run(`UPDATE ofertas SET plazas_disponibles = ?, plazas_concedidas = ? 
+                WHERE ofertas.asignatura = ?`, [
+                    parseInt(modificacion.disponibles) +1,
+                    parseInt(modificacion.concedidas) -1,
+                    modificacion.asignatura
+                ], (err) => {
+            if (err) {
+                jsonResponse(500, { 
+                    error: `Cannot updates: ${err}`
+                })
+                return
+            }
+            console.log(modificacion.disponibles, modificacion.concedidas, modificacion.asignatura)
+            jsonResponse(200, {
+                error: null,
+            })
+        })
+    })
+
+}
