cc9d4a40c94d7566583955e1b990ca25bc352740
jeanmisslin <jeanmisslindj@gmail.com>
2019-12-27 18:28:45 +0100

    Actualización de la Base de Datos

--- database/tfg.sql
+++ database/tfg.sql
@@ -83,7 +83,7 @@ CREATE TABLE asignaciones(
     id_asignacion INTEGER PRIMARY KEY,
     acuerdo_academico int,
     oferta int,
-    estado text,
+    estado_solicitud text,
     FOREIGN KEY (acuerdo_academico) REFERENCES acuerdos_academicos (id_acuerdo),
     FOREIGN KEY (oferta) REFERENCES ofertas (id_oferta),
     CONSTRAINT UC_ASIGNACION UNIQUE (acuerdo_academico, oferta)
