bea6f891790e67d4758520d2b3be4b4ebe6b07a7
jeanmisslin <jeanmisslindj@gmail.com>
2020-01-05 18:55:29 +0100

    Componentes de visualización
    - Nueva visualización de los datos de las ofertas de una asignatura

---
+++ src/components/MostrarTitulacionesOfertas.svelte
@@ -0,0 +1,55 @@
+<script>
+  export let ofertas;
+  export let periodo;
+  export let asignatura;
+  export let titulaciones;
+
+  import ModificaTitulacionOferta from "./ModificaTitulacionOferta.svelte";
+  import EliminarOferta from "./EliminarOferta.svelte";
+
+</script>
+
+<style>
+  #tabla {
+    border-collapse: collapse;
+    width: 100%;
+    margin-bottom: 10px;
+    margin-top: 25px;
+  }
+
+  td {
+    border: 1px solid black;
+    text-align: center;
+    padding: 3px;
+  }
+
+  th {
+    border: 1px solid black;
+    text-align: center;
+    padding: 3px;
+    background-color: rgb(117, 182, 226);
+    color: black;
+  }
+
+  tr:nth-child(even) {
+    background-color: rgb(255, 246, 239);
+  }
+</style>
+
+<table id="tabla">
+    <tr>
+      <th>TITULACION</th>
+      <th>MODIFICAR</th>
+      <th>ELIMINAR</th>
+    </tr>
+    {#each ofertas as o}
+    <tr>
+    {#if o.periodo_academico === periodo && o.asignatura === asignatura}
+      <td>{o.titulacion_ingles}</td>
+      <td><ModificaTitulacionOferta oferta={o} {titulaciones}/></td>
+      <td><EliminarOferta oferta={o.id_oferta}/></td>
+      {/if}
+      </tr>
+    {/each}
+  </table>
+  
