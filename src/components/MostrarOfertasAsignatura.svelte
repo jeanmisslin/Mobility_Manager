cf7e919b6aa7fc813b6e72ee8f953c5cea7f34d9
jeanmisslin <jeanmisslindj@gmail.com>
2020-01-07 20:02:15 +0100

    Componente que muestra las plazas y las titulaciones en las que se ofrece una asignatura.

---
+++ src/components/MostrarOfertasAsignatura.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let ofertas;
+  export let periodo;
+  export let asignatura;
+  export let titulaciones;
+  export let asignaciones;
+
+  import MostrarTitulacionesOfertas from "./MostrarTitulacionesOfertas.svelte";
+  import AñadirOferta from "./AñadirOferta.svelte";
+  import ModificarPlazasOferta from "./ModificarPlazasOferta.svelte";
+
+  let oferta = ofertas.find(
+    element =>
+      element.asignatura === asignatura &&
+      element.periodo_academico === periodo.id_periodo
+  );
+
+  let { id_periodo } = periodo;
+
+  let message;
+</script>
+
+<style>
+  #periodo {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 25px;
+    margin-bottom: 10px;
+    height: 30px;
+    width: 25%;
+    font-weight: 650;
+    background-color: rgb(240, 204, 175);
+    color: black;
+    border: 1px solid black;
+  }
+</style>
+
+{#if oferta === null || oferta === undefined}
+  <div id="periodo">
+    <div>Q{periodo.cuatrimestre}, {periodo.año}</div>
+  </div>
+  <AñadirOferta {asignatura} {titulaciones} periodo={id_periodo} {ofertas} {asignaciones}/>
+{:else}
+  <ModificarPlazasOferta {oferta} {ofertas}/>
+  <MostrarTitulacionesOfertas
+    {ofertas}
+    {asignatura}
+    periodo={id_periodo}
+    {titulaciones}
+    {asignaciones} />
+{/if}
