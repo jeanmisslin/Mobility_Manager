132c3fb7a8da54c5494b3c01b38038cd0a325464
jeanmisslin <jeanmisslindj@gmail.com>
2020-01-14 11:21:20 +0100

    Componente TablaFiltrableComponentes
    - Esta nueva versión del componente TablaFiltrable permite añadir el botón solicitar o modificar para cada linea de la tabla.

---
+++ src/components/TablaFiltrableComponentes.svelte
@@ -0,0 +1,122 @@
+<script>
+  export let campos; // campos que se miran al filtrar
+  export let tabla; // Array de objetos javascript con todos los datos
+  export let componente;
+  export let acuerdo;
+
+  import SolicitarAsignatura from "./SolicitarAsignatura.svelte";
+  import ModificarEstadoAsignacion from "./ModificarEstadoAsignacion.svelte";
+
+  let filtro = "";
+
+  // TODO: Comprobar que todos los objetos tienen los mismos campos
+  //       (De momentos asumimos que no)
+
+  const strIn = (a, b) => a.toLowerCase().indexOf(b.toLowerCase()) != -1;
+
+  $: tablaFiltrada = tabla.filter(obj => {
+    for (let c of campos) {
+      if (!(c.name in obj)) {
+        console.error(`${c.name} no está en ${JSON.stringify(obj)}`);
+      }
+      if (c.nombre) {
+        {
+          c.nombre.toUpperCase();
+        }
+      }
+      if (c.filter) {
+        if (strIn(obj[c.name], filtro)) return true;
+      }
+    }
+    return false;
+  });
+</script>
+
+<style>
+  #tabla {
+    border-collapse: collapse;
+    width: 100%;
+    margin-bottom: 0px;
+    margin-top: 0px;
+  }
+
+  td {
+    border: 1px solid black;
+    text-align: center;
+    padding: 3px;
+  }
+
+  th {
+    border: 1px solid black;
+    text-align: center;
+    padding: 3px;
+    background-color: rgb(117, 182, 226);
+    color: black;
+  }
+
+  tr:nth-child(even) {
+    background-color: rgb(255, 246, 239);
+  }
+
+  #scroll {
+    overflow: scroll;
+    margin-top: 20px;
+    height: 204px;
+    width: 100%;
+    border: 1px solid black;
+  }
+</style>
+
+<div id="filtro">
+  <p>
+    BUSCADOR:
+    <input
+      type="text"
+      bind:value={filtro}
+      placeholder="Introduce la palabra clave"
+      title="Type in a name" />
+  </p>
+</div>
+
+<div id="scroll">
+  <table id="tabla">
+    <tr>
+      {#each campos as c}
+        {#if c.show}
+          <th>
+            {#if c.nombre}
+              {c.nombre.toUpperCase()}
+            {:else}{c.name.toUpperCase()}{/if}
+          </th>
+        {/if}
+      {/each}
+      {#if componente === 'solicitar'}
+        <th>SOLICITAR</th>
+      {:else if componente === 'modificar'}
+        <th>MODIFICAR</th>
+      {/if}
+    </tr>
+    {#each tablaFiltrada as obj}
+      <tr>
+        {#each campos as c}
+          {#if c.show}
+            <td>
+              {#if c.render}
+                {@html c.render(obj)}
+              {:else}{obj[c.name]}{/if}
+            </td>
+          {/if}
+        {/each}
+        {#if componente === 'solicitar'}
+          <td>
+            <SolicitarAsignatura acuerdo={acuerdo.id_acuerdo} oferta={obj} />
+          </td>
+        {:else if componente === 'modificar'}
+          <td>
+            <ModificarEstadoAsignacion asignatura={obj} {acuerdo} />
+          </td>
+        {/if}
+      </tr>
+    {/each}
+  </table>
+</div>
