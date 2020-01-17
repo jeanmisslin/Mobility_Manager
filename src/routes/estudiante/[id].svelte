b6d85b68c506b5fc225c2e02206fa737b32faee1
jeanmisslin <jeanmisslindj@gmail.com>
2020-01-17 11:18:21 +0100

    Modificación de código
    - Incorpora el nuevo componente Acuerdo.
    - Incorpora un nuevo SELECT aunque no lo utiliza por que da errores con el componente SolicitarAsignatura.

--- src/routes/estudiante/[id].svelte
+++ src/routes/estudiante/[id].svelte
@@ -19,12 +19,16 @@
     let { asignaciones } = await this.fetch(`asignaciones.json`).then(body =>
       body.json()
     );
+    let { unaofertaporasignatura } = await this.fetch(`unaofertaporasignatura.json`).then(body =>
+      body.json()
+    );
     return {
       estudiante,
       universidades,
       titulaciones,
       ofertas,
       asignaciones,
+      unaofertaporasignatura,
       periodos
     };
   }
@@ -34,11 +38,13 @@
   import ModificaEstudiante from "../../components/ModificaEstudiante.svelte";
   import ModificaAcuerdo from "../../components/ModificaAcuerdo.svelte";
   import TablaFiltrableComponentes from "../../components/TablaFiltrableComponentes.svelte";
+  import Acuerdo from "../../components/Acuerdo.svelte";
 
   export let estudiante;
   export let universidades;
   export let titulaciones;
   export let ofertas;
+  export let unaofertaporasignatura;
   export let periodos;
   export let asignaciones;
 
@@ -121,47 +127,7 @@
     margin-top: 10px;
   }
 
-  #solicitadas {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 25px;
-    margin-bottom: 10px;
-    height: 30px;
-    width: 50%;
-    font-weight: 650;
-    background-color: rgb(240, 204, 175);
-    color: black;
-    border: 1px solid black;
-  }
-
-  #recomendadas {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 25px;
-    margin-bottom: 10px;
-    height: 30px;
-    width: 50%;
-    font-weight: 650;
-    background-color: rgb(240, 204, 175);
-    color: black;
-    border: 1px solid black;
-  }
-
-  #ofertadas {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 25px;
-    margin-bottom: 10px;
-    height: 30px;
-    width: 50%;
-    font-weight: 650;
-    background-color: rgb(240, 204, 175);
-    color: black;
-    border: 1px solid black;
-  }
+  
 </style>
 
 <svelte:head>
@@ -186,29 +152,6 @@
 <div id="acuerdos">ACUERDOS ACADEMICOS</div>
 
 {#each acuerdos as acuerdo}
-  <ModificaAcuerdo {periodos} {acuerdo} {titulaciones} />
-
-  <div id="solicitadas">ASIGNATURAS SOLICITADAS</div>
-
-  <TablaFiltrableComponentes
-    tabla={asignaturas}
-    {acuerdo}
-    componente="modificar"
-    campos={[{ name: 'codigo_asignatura', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, { name: 'nombre_catalan', filter: true }, { name: 'nombre_castellano', filter: true }, { name: 'idioma', show: true, filter: true }, { name: 'ects', show: true }, { name: 'estado_solicitud', nombre: 'estado', show: true }]} />
-
-  <div id="recomendadas">ASIGNATURAS RECOMENDADAS</div>
-
-  <TablaFiltrableComponentes
-    tabla={ofertas.filter(ofer => ofer.periodo_academico === acuerdo.periodo_academico && ofer.titulacion === acuerdo.titulacion)}
-    {acuerdo}
-    componente="solicitar"
-    campos={[{ name: 'codigo_asignatura', nombre: 'código', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, { name: 'nombre_catalan', filter: true }, { name: 'nombre_castellano', filter: true }, { name: 'idioma', show: true, filter: true }, { name: 'ects', show: true }, { name: 'plazas_disponibles', nombre: 'plazas', show: true }]} />
-
-  <div id="ofertadas">ASIGNATURAS OFERTADAS</div>
-
-  <TablaFiltrableComponentes
-    tabla={ofertas.filter(ofer => ofer.periodo_academico === acuerdo.periodo_academico)}
-    {acuerdo}
-    componente="solicitar"
-    campos={[{ name: 'codigo_asignatura', nombre: 'código', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, { name: 'nombre_catalan', filter: true }, { name: 'nombre_castellano', filter: true }, { name: 'titulacion_ingles', nombre: 'titulación', show: true, filter: true }, { name: 'idioma', show: true, filter: true }, { name: 'ects', show: true }, { name: 'plazas_disponibles', nombre: 'plazas', show: true }]} />
+<Acuerdo {periodos} {acuerdo} {titulaciones} {asignaturas} {ofertas} {unaofertaporasignatura}/>
 {/each}
+
