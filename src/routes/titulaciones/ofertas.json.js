b429432fc59f5b88291493738466e9b48cf8560a
jeanmisslin <jeanmisslindj@gmail.com>
2020-01-24 10:30:07 +0100

    Modificación del SELECT

--- src/routes/titulaciones/ofertas.json.js
+++ src/routes/titulaciones/ofertas.json.js
@@ -7,11 +7,11 @@ export function get(req, res, next) {
         res.end(JSON.stringify(obj))
     }
 
-    db.all(`SELECT asignaturas.*, ofertas.*, periodos_academicos.* , titulaciones.*
-    FROM asignaturas INNER JOIN ofertas, periodos_academicos, titulaciones
+    db.all(`SELECT ofertas.*, asignaturas.*, periodos_academicos.*
+    FROM ofertas INNER JOIN asignaturas, periodos_academicos
     ON asignaturas.codigo_asignatura = ofertas.asignatura
     AND periodos_academicos.id_periodo = ofertas.periodo_academico
-    AND titulaciones.codigo_titulacion = ofertas.titulacion ORDER BY ofertas.asignatura`, (err, ofertas) => {
+    ORDER BY ofertas.asignatura`, (err, ofertas) => {
         if (err) {
             console.log(err)
             jsonResponse(500, { error: `No se pueden consultar ofertas: ${err}` })
