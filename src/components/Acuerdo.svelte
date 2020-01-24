b89cdce0381bd0ba04a419bc43053e877fa663be
jeanmisslin <jeanmisslindj@gmail.com>
2020-01-24 10:19:32 +0100

    Funciones añadidas
    - Se añade la función econtrartitulacion() para comprobar si el código de la titulación del acuerdo académico aparece en el string oferta.titulacion
    - Se añade la función asignada para comprobar si una oferta ya ha sido asignada a ese estudiante y, en caso afirmativo, no se muestra ni en la tabla de asignaturas recomendadas y en la tabla de asignaturas totales.

--- src/components/Acuerdo.svelte
+++ src/components/Acuerdo.svelte
@@ -8,6 +8,23 @@
 
   import ModificaAcuerdo from "./ModificaAcuerdo.svelte";
   import TablaFiltrableComponentes from "./TablaFiltrableComponentes.svelte";
+
+  function encontrartitulacion(a, t) {
+    let array = a.split(",");
+    let i;
+    for (i = 0; i != a.length; i++)
+      if (array[i] === t) {
+        return true;
+      }
+    return false;
+  }
+  
+  function asignada(a, o) {
+    let asignacion = asignaturas.find(
+      element => element.oferta === o.id_oferta
+    );
+    return asignacion;
+  }
 </script>
 
 <style>
@@ -56,47 +73,26 @@
 
 <ModificaAcuerdo {periodos} {acuerdo} {titulaciones} />
 
-  <div id="solicitadas">ASIGNATURAS SOLICITADAS</div>
+<div id="solicitadas">ASIGNATURAS SOLICITADAS</div>
 
-  <TablaFiltrableComponentes
-    tabla={asignaturas}
-    {acuerdo}
-    componente="modificar"
-    campos={[
-      { name: 'codigo_asignatura', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, 
-      { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, 
-      { name: 'nombre_catalan', filter: true }, { name: 'nombre_castellano', filter: true }, 
-      { name: 'idioma', show: true, filter: true }, 
-      { name: 'ects', show: true }, 
-      { name: 'estado_solicitud', nombre: 'estado', show: true }]} />
+<TablaFiltrableComponentes
+  tabla={asignaturas}
+  {acuerdo}
+  componente="modificar"
+  campos={[{ name: 'codigo_asignatura', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, { name: 'nombre_catalan', filter: true }, { name: 'nombre_castellano', filter: true }, { name: 'idioma', show: true, filter: true }, { name: 'ects', show: true }, { name: 'estado_solicitud', nombre: 'estado', show: true }]} />
 
-  <div id="recomendadas">ASIGNATURAS RECOMENDADAS</div>
+<div id="recomendadas">ASIGNATURAS RECOMENDADAS</div>
 
-  <TablaFiltrableComponentes
-    tabla={ofertas.filter(ofer => ofer.periodo_academico === acuerdo.periodo_academico && ofer.titulacion === acuerdo.titulacion)}
-    {acuerdo}
-    componente="solicitar"
-    campos={[
-      { name: 'codigo_asignatura', nombre: 'código', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, 
-      { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, 
-      { name: 'nombre_catalan', filter: true }, 
-      { name: 'nombre_castellano', filter: true }, 
-      { name: 'idioma', show: true, filter: true }, 
-      { name: 'ects', show: true }, 
-      { name: 'plazas_disponibles', nombre: 'plazas', show: true }]} />
+<TablaFiltrableComponentes
+  tabla={ofertas.filter(ofer => ofer.periodo_academico === acuerdo.periodo_academico && encontrartitulacion(ofer.titulacion, acuerdo.titulacion) && (asignada(asignaturas, ofer) === null || asignada(asignaturas, ofer) === undefined))}
+  {acuerdo}
+  componente="solicitar"
+  campos={[{ name: 'codigo_asignatura', nombre: 'código', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, { name: 'nombre_catalan', filter: true }, { name: 'nombre_castellano', filter: true }, { name: 'idioma', show: true, filter: true }, { name: 'ects', show: true }, { name: 'plazas_disponibles', nombre: 'plazas', show: true }]} />
 
-  <div id="ofertadas">ASIGNATURAS OFERTADAS</div>
+<div id="ofertadas">ASIGNATURAS OFERTADAS</div>
 
-  <TablaFiltrableComponentes
-    tabla={ofertas.filter(ofer => ofer.periodo_academico === acuerdo.periodo_academico)}
-    {acuerdo}
-    componente="solicitar"
-    campos={[
-      { name: 'codigo_asignatura', nombre: 'código', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, 
-      { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, 
-      { name: 'nombre_catalan', filter: true }, 
-      { name: 'nombre_castellano', filter: true }, 
-      { name: 'titulacion_ingles', nombre: 'titulacion', show: true, filter: true }, 
-      { name: 'idioma', show: true, filter: true }, 
-      { name: 'ects', show: true }, 
-      { name: 'plazas_disponibles', nombre: 'plazas', show: true }]} />
+<TablaFiltrableComponentes
+  tabla={ofertas.filter(ofer => ofer.periodo_academico === acuerdo.periodo_academico && (asignada(asignaturas, ofer) === null || asignada(asignaturas, ofer) === undefined))}
+  {acuerdo}
+  componente="solicitar"
+  campos={[{ name: 'codigo_asignatura', nombre: 'código', show: true, filter: true, render: obj => `<a href="/asignatura/${obj.codigo_asignatura}">${obj.codigo_asignatura}</a>` }, { name: 'nombre_ingles', nombre: 'titulo', show: true, filter: true, render: obj => `<a href="${obj.plan_de_estudios_ingles}">${obj.nombre_ingles}</a>` }, { name: 'nombre_catalan', filter: true }, { name: 'nombre_castellano', filter: true }, { name: 'idioma', show: true, filter: true }, { name: 'ects', show: true }, { name: 'plazas_disponibles', nombre: 'plazas', show: true }]} />
