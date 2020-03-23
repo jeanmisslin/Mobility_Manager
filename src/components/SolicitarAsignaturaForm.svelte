1385e1bac0af3769981fe091818a122a5f07dac5
jeanmisslin <jeanmisslindj@gmail.com>
2020-03-23 19:22:39 +0100

    Componente que permite solicitar la asignatura de la fila correspondiente.

---
+++ src/components/SolicitarAsignaturaForm.svelte
@@ -0,0 +1,122 @@
+<script>
+  export let oferta;
+  export let acuerdo;
+
+  import MenuSurface, { Anchor } from "@smui/menu-surface";
+  import IconButton from "@smui/icon-button";
+  import Select, { Option } from "@smui/select";
+  import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text/index";
+  import Dialog, { Title, Content, Actions, InitialFocus } from "@smui/dialog";
+  import Button, {
+    Group,
+    GroupItem,
+    Label,
+    Icon as ButtonIcon
+  } from "@smui/button";
+  import List, { Item, Graphic, Text } from "@smui/list";
+  import { MDCDialog } from "@material/dialog";
+
+  let simpleDialog;;
+
+  let message;
+  let asignacion;
+  let modificacion;
+
+  $: asignacion = {
+    open: false,
+    acuerdo: acuerdo,
+    oferta: oferta.id_oferta,
+    estado: "Solicitada"
+  };
+
+  $: modificacion = {
+    solicitadas: oferta.plazas_solicitadas,
+    asignatura: oferta.asignatura,
+    periodo: oferta.periodo_academico
+  };
+
+  function asignar() {
+    fetch(`nuevaasignacion.json`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(asignacion)
+    })
+      .then(body => body.json())
+      .then(json => {
+        if (json.error) {
+          message = json.error;
+        } else {
+          message = "asignacion guardada";
+        }
+      });
+  }
+
+  function solicitar() {
+    fetch(`solicitarasignatura.json`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(modificacion)
+    })
+      .then(body => body.json())
+      .then(json => {
+        if (json.error) {
+          message = json.error;
+        } else {
+          message = "oferta guardada";
+        }
+      });
+  }
+
+  function ejecutarambas() {
+    console.log("Click!!");
+    asignar();
+    solicitar();
+  }
+
+  function abrir() {
+    asignacion = { ...asignacion, open: true };
+  }
+
+  function cerrar() {
+    asignacion = { ...asignacion, open: false };
+  }
+</script>
+
+<style>
+
+</style>
+
+<!--{#if asignacion.open}
+  <div id="buttons">
+    <button on:click={ejecutarambas}>Salvar</button>
+    <button on:click={cerrar}>Cancelar</button>
+  </div>
+  <div>
+    {#if message}
+      <p>{message}</p>
+    {/if}
+  </div>
+{:else}
+  <div id="buttons">
+    <div id="field">
+      <button on:click={abrir}>Solicitar</button>
+    </div>
+  </div>
+{/if}-->
+
+<div>
+    <Dialog bind:this={simpleDialog} aria-labelledby="simple-title" aria-describedby="simple-content">
+      <Title id="simple-title">Solicitar Asignatura</Title>
+      <Actions>
+        <Button color="secondary" variant="raised" on:click={cerrar}>
+          <Label>NO</Label>
+        </Button>
+        <Button color="secondary" variant="raised" on:click={ejecutarambas}>
+          <Label>SI</Label>
+        </Button>
+      </Actions>
+    </Dialog>
+
+    <Button color="primary" variant="raised" on:click={() => simpleDialog.open()}><Label>Solicitar</Label></Button>
+  </div>
