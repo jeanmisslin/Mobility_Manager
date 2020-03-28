033402c28cada2e2dd0013dcd5532d193d10ff68
jeanmisslin <jeanmisslindj@gmail.com>
2020-03-28 12:07:30 +0100

    Json que ejecuta un SELECT en la tabla de universidades

---
+++ src/routes/universidad/[id].json.js
@@ -0,0 +1,20 @@
+
+import { db } from '../../db'
+
+export function get(req, res, next) {
+    const id = req.params.id;
+
+    const jsonResponse = (code, obj) => {
+        res.writeHead(code, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(obj))
+    }
+
+    db.get(`SELECT * FROM universidades WHERE codigo_universidad = ?`, [id], (err, universidad)=> {
+        if(err) {
+            console.log(err)
+            jsonResponse(500, { error: `No se puede realizar la consulta: ${err}` })
+            return
+        }
+        jsonResponse(200, universidad);
+    })
+}
