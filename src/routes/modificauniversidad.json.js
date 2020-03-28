501e3aaf341d744a89306fba9b96d96f4de08664
jeanmisslin <jeanmisslindj@gmail.com>
2020-03-28 12:06:56 +0100

    Json que ejecuta un UPDATE en la tabla de universidades.

---
+++ src/routes/modificauniversidad.json.js
@@ -0,0 +1,34 @@
+
+import { db } from '../db'
+
+export function post(req, res, next) {
+    const jsonResponse = (code, obj) => {
+        res.writeHead(code, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(obj))
+    }
+
+    let body = '';
+    req.on('data', data => body += data)
+    req.on('end', () => {
+        const modificauniversidad = JSON.parse(body)
+        db.run(`UPDATE universidades 
+                SET codigo_universidad = ?, universidad = ?, pais = ? 
+                WHERE universidades.codigo_universidad = ?`, [
+                    modificauniversidad.codigo_universidad,
+                    modificauniversidad.universidad,
+                    modificauniversidad.pais,
+                    modificauniversidad.codigo_ant
+                ], (err) => {
+            if (err) {
+                jsonResponse(500, { 
+                    error: `No se puede modificar la universidad: ${err}`
+                })
+                return
+            }
+            jsonResponse(200, {
+                error: null,
+            })
+        })
+    })
+
+}
